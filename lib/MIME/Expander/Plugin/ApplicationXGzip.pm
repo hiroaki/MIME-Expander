ecd1d0f5acbe81559eaab0b266db05e91024effc
WATANABE Hiroaki <hwat@mac.com>
2013-03-23 17:06:56 +0900

    Add plugin for application/x-gzip

---
+++ lib/MIME/Expander/Plugin/ApplicationXGzip.pm
@@ -0,0 +1,66 @@
+package MIME::Expander::Plugin::ApplicationXGzip;
+
+use strict;
+use warnings;
+use vars qw($VERSION);
+$VERSION = '0.01';
+
+use parent qw(MIME::Expander::Plugin);
+__PACKAGE__->mk_classdata('ACCEPT_TYPES' => [qw(
+    application/x-gzip
+    )]);
+
+use IO::Uncompress::Gunzip;
+
+sub expand {
+    my $self        = shift;
+    my $contents    = shift;
+    my $callback    = shift;
+    my $c           = 0;
+
+    my $z = IO::Uncompress::Gunzip->new(\$contents, Append => 1) 
+        or die "gunzip failed: $IO::Uncompress::Gunzip::GunzipError";
+
+    my $buf;
+    1 while( 0 < $z->read($buf) );
+    
+    my $h = $z->getHeaderInfo || {};
+    $callback->( \$buf, {
+        filename => $h->{Name},
+        } ) if( ref $callback eq 'CODE' );
+    ++$c;
+
+    return $c;
+}
+
+1;
+__END__
+
+
+=pod
+
+=head1 NAME
+
+MIME::Expander::Plugin::ApplicationXGzip - a plugin for MIME::Expander
+
+=head1 SYNOPSIS
+
+    my $expander = MIME::Expander::Plugin::ApplicationXGzip->new;
+    $expander->expand(\$data, sub {
+            my $ref_expanded_data = shift;
+            my $metadata = shift || {};
+            print $metadata->{content_type}, "\n";
+            print $metadata->{filename}, "\n";
+        });
+
+=head1 DESCRIPTION
+
+Expand data that media type is "application/x-gzip".
+
+=head1 SEE ALSO
+
+L<MIME::Expander>
+L<MIME::Expander::Plugin>
+L<IO::Uncompress::Gunzip>
+
+=cut
