01b9cfc78f62fe2c4e29fbd9a9ea4550e66e1e5d
WATANABE Hiroaki <hwat@mac.com>
2013-03-26 01:49:25 +0900

    Refactoring, Improve document

--- lib/MIME/Expander.pm
+++ lib/MIME/Expander.pm
@@ -8,10 +8,9 @@ $VERSION = '0.01';
 use vars qw($DEBUG);
 $DEBUG = 0;
 
-use File::MMagic;
 use Email::MIME;
 use Email::MIME::ContentType ();
-use Module::Load qw(load);
+use Module::Load;
 use Module::Pluggable sub_name => 'expanders';
 
 my $PrefixPlugin   = 'MIME::Expander::Plugin';
@@ -24,9 +23,9 @@ sub import {
 
 sub canonical_content_type {
     return undef unless( defined $_[1] );
-    my $data = Email::MIME::ContentType::parse_content_type($_[1]);
-    if( $data->{discrete} and $data->{composite} ){
-        return join('/',$data->{discrete}, $data->{composite});
+    my $ct = Email::MIME::ContentType::parse_content_type($_[1]);
+    if( $ct->{discrete} and $ct->{composite} ){
+        return join('/',$ct->{discrete}, $ct->{composite});
     }
     return undef;
 }
@@ -41,11 +40,11 @@ sub new {
     my $class = shift;
     $class = ref $class || $class;
     my $self = {
-        expects => [],
-        guess_type => undef,
-        depth   => undef,
+        expects     => [],
+        guess_type  => undef,
+        depth       => undef,
         };
-    bless $self, $class;
+    bless  $self, $class;
     return $self->init(@_);
 }
 
@@ -77,12 +76,7 @@ sub expects {
         die "setting value is not acceptable, it requires an reference of ARRAY"
             if( defined $self->{expects} and ref($self->{expects}) ne 'ARRAY' );
     }
-    $self->{expects};
-}
-
-sub depth {
-    my $self = shift;
-    return @_ ? $self->{depth} = shift : $self->{depth};
+    return $self->{expects};
 }
 
 sub is_expected {
@@ -94,6 +88,16 @@ sub is_expected {
     return ();
 }
 
+sub depth {
+    my $self = shift;
+    if( @_ ){
+        $self->{depth} = shift;
+        die "setting value is not acceptable, it requires a native number"
+            if( defined $self->{depth} and $self->{depth} =~ /\D/ );
+    }
+    return $self->{depth};
+}
+
 sub guess_type {
     my $self = shift;
     if( @_ ){
@@ -101,23 +105,24 @@ sub guess_type {
         die "setting value is not acceptable, it requires an reference of CODE"
             if( defined $self->{guess_type} and ref($self->{guess_type}) ne 'CODE' );
     }
-    $self->{guess_type};
+    return $self->{guess_type};
 }
 
 sub guess_type_default {
-    File::MMagic->new->checktype_contents(${$_[1]});
+    Module::Load::load File::MMagic;
+    return File::MMagic->new->checktype_contents(${$_[1]});
 }
 
 sub guess_type_by_contents {
     my $self     = shift;
     my $ref_data = shift or die "missing mandatory parameter";
-    my $meta     = shift || {};
+    my $info     = shift || {};
     
     my $type;
     if( ref($self->guess_type) eq 'CODE' ){
-        $type = $self->guess_type->($ref_data, $meta);
+        $type = $self->guess_type->($ref_data, $info);
     }else{
-        $type = $self->guess_type_default($ref_data, $meta);
+        $type = $self->guess_type_default($ref_data, $info);
     }
     return ($type || 'application/octet-stream');
 }
@@ -144,7 +149,7 @@ sub plugin_for {
             next unless( $klass );
         }
         
-        load $klass;
+        Module::Load::load $klass;
         if( $klass->accepts( $type ) ){
             $plugin = $klass->new;
             last;
@@ -156,18 +161,18 @@ sub plugin_for {
 sub _create_media {
     my $self     = shift;
     my $ref_data = shift or die "missing mandatory parameter";
-    my $meta     = shift || {};
+    my $info     = shift || {};
 
-    my $type = $self->canonical_content_type($meta->{content_type});
+    my $type = $self->canonical_content_type($info->{content_type});
     if( ! $type or $type =~ m'^application/octet-?stream$' ){ #'
-        $type = $self->guess_type_by_contents($ref_data, $meta);
+        $type = $self->guess_type_by_contents($ref_data, $info);
     }
 
-    Email::MIME->create(
+    return Email::MIME->create(
         attributes => {
             content_type    => $type,
             encoding        => 'binary',
-            filename        => $meta->{filename},
+            filename        => $info->{filename},
             },
         body => $$ref_data,
         );
@@ -175,21 +180,22 @@ sub _create_media {
 
 sub walk {
     my $self        = shift;
-    my $contents    = shift;
+    my $data        = shift;
     my $callback    = shift;
-    my $meta        = shift || {};
+    my $info        = shift || {};
     my $c           = 0;
 
     my @medias = ($self->_create_media(
-        ref $contents eq 'SCALAR' ? $contents : \$contents,
-        $meta));
+        ref $data eq 'SCALAR' ? $data : \$data,
+        $info));
 
-    # flatten contents contains
+    # reset vars for depth option
     my $ptr     = 0;
     my $limit   = 0;
     my $level   = 1;
     my $bound   = scalar @medias;
-    $self->debug("initial bound=[$bound]");
+    
+    # when expandable contents, then append it to @medias
     while( my $media = shift @medias ){
         $self->debug("====> shift media, remains=[@{[ scalar @medias ]}]");
 
@@ -198,8 +204,8 @@ sub walk {
         $self->debug("* type is [$type], plugin_for [@{[ $plugin || '' ]}]");
 
         if( $limit or $self->is_expected( $type ) or ! $plugin ){
-            # expected or un-expandable contents
-            $self->debug("=> limited, expected or un-expandable contents");
+            # expected or un-expandable data
+            $self->debug("=> limited, expected or un-expandable data");
             $callback->($media) if( ref $callback eq 'CODE' );
             ++$c;
         }else{
@@ -212,7 +218,7 @@ sub walk {
         }
 
         ++$ptr;
-        $self->debug("increment ptr=[$ptr] | bound=[$bound] level=[$level]");
+        $self->debug("incremented ptr=[$ptr] | bound=[$bound] level=[$level]");
         if( $bound <= $ptr ){
             
             if( $self->depth and $self->depth <= $level ){
@@ -220,9 +226,8 @@ sub walk {
                 $self->debug("set limit to TRUE");
             }
             $bound += scalar @medias;
-            $self->debug("update bound=[$bound]");
             ++$level;
-            $self->debug("update level=[$level]");
+            $self->debug("updated bound=[$bound] level=[$level]");
         }
     }
     
@@ -232,6 +237,9 @@ sub walk {
 
 1;
 __END__
+
+
+=pod
 
 =head1 NAME
 
@@ -264,49 +272,142 @@ MIME::Expander - Expands archived, compressed or multi-parted file by MIME mecha
 
 MIME::Expander is an utility module that expands archived, compressed or multi-parted file by MIME mechanism.
 
-=head1 CONSTRUCTOR
+=head1 CONSTRUCTOR AND ACCESSORS
 
 The constructor new() creates an instance, and accepts a reference of hash as configurations.
+
+Following key of hash are available, and there is an access method of a same name.
+
+=over 4
+
+=item expects
+
+A value is a list reference and the elements are string or regular expression.
+
+If this parameter is set, then the walk() will not expand contents of specified mime types.
+
+=item guess_type
+
+A value is a code reference.
+
+The routine have to determine the type of the data which will be inputted.
+
+The parameters passed to the routine are a reference of scalar to contents, 
+and information as reference of hash. 
+
+Although the information may have a "filename",
+however depending on implements of each expander module, it may not be expectable.
+
+The routine have to return mime type string, or undef.
+If value of return is false value, that means "application/octet-stream".
+
+For example, sets routine which determine text or jpeg.
+
+    my $exp = MIME::Expander->new({
+        guess_type => sub {
+                my $ref_contents = shift;
+                my $info         = shift || {};
+                if( defined $info->{filename} ){
+                    my ($suffix) = $info->{filename} =~ /\.(.+)$/;
+                    if( defined $suffix ){
+                        if( lc $suffix eq 'txt' ){
+                            return 'text/plain';
+                        }elsif( $suffix =~ /^jpe?g$/i ){
+                            return 'image/jpeg';
+                        }
+                    }
+                }
+            },
+        });
+
+There is default routine that will call guess_type_default() method.
+
+=item depth
+
+A value is a native number.
+
+Please see "walk".
+
+=back
 
 =head1 CLASS METHODS
 
 =head2 canonical_content_type( $content_type )
 
-TODO
+This is an utility for unifying header "Content-type" with parameters.
+
+    MIME::Expander->canonical_content_type(
+        "text/plain; charset=ISO-2022-JP");
+    #=> "text/plain"
 
 =head1 INSTANCE METHODS
 
 =head2 init
 
-TODO
+Initialize instance. This is for override.
 
 =head2 expects( \@list )
 
-TODO
+Accessor to field "expects".
 
 =head2 is_expected( $type )
 
-TODO
+Is $type the contents set to field "expects" ?
+
+=head2 depth( $native_number )
+
+Accessor to field "depth".
 
 =head2 guess_type( \&code )
 
-TODO
+Accessor to field "guess_type".
 
 =head2 guess_type_default( \$contents )
 
-TODO
+It is called when the field "guess_type" is not set.
 
-=head2 guess_type_by_contents( \$contents, \%meta )
+=head2 guess_type_by_contents( \$contents, \%info )
 
-TODO
+The routine "guess_type" actually determines mime type.
+
+Optional %info is as hint for determing mime type.
+It will be passed to a "guess_type" routine directly.
+
+"filename" can be included in %info.
 
 =head2 plugin_for( $type )
 
 TODO
 
-=head2 walk( $contents, $callback )
+=head2 walk( \$data, $callback )
 
-TODO
+If the $data which can be expanded in the inputted data exists,
+it will be expanded and passed to callback.
+
+The expanded data are further checked and processed recursively.
+
+The recursive depth is to the level of the value of "depth" field.
+
+A media object which is a L<Email::MIME>, is passed to the callback routine, 
+They are the results of this module. 
+
+As the work, it sometimes often saves.
+However the file name may not be obtained with the specification of expander module.
+But it may be used, since it is set to "filename" attribute when it exists.
+
+    $me->expand( \$data, sub {
+            my $email = shift;
+            my $name  = $email->invent_filename;
+            open my $fh, ">$name" or die;
+            $fh->print($email->body_raw);
+            close $fh;
+        });
+
+See also L<Email::MIME> for $email object.
+
+Only this $email object has rules on this MIME::Expander utility class.
+The expanded data is set to "body" and the Content-Transfer-Encoding is "binary".
+Therefore, in order to take out the expanded contents, please use "body_raw" method.
 
 =head1 IMPORT
 
@@ -314,7 +415,7 @@ TODO
 
 =head1 PLUGIN
 
-TODO - See also L<MIME::Expander::Plugin>
+TODO - See also L<MIME::Expander::Plugin>.
 
 =head1 CAVEATS
 
